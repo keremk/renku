c5b236a6e59dda6776201ee08b2c22f55cfa8843
Kerem Karatal <kkaratal@gmail.com>
2025-12-22 12:07:13 +0100

    fix: refactor the web site and streamline styling and icons

---
+++ web/src/components/icons/index.ts
@@ -0,0 +1,11 @@
+// Icon components barrel export
+export { default as IconGitHub } from './IconGitHub.astro';
+export { default as IconDiscord } from './IconDiscord.astro';
+export { default as IconMenu } from './IconMenu.astro';
+export { default as IconArrowRight } from './IconArrowRight.astro';
+export { default as IconHeart } from './IconHeart.astro';
+export { default as IconChevronDown } from './IconChevronDown.astro';
+export { default as IconSparkles } from './IconSparkles.astro';
+export { default as IconTemplate } from './IconTemplate.astro';
+export { default as IconPlug } from './IconPlug.astro';
+export { default as IconTerminal } from './IconTerminal.astro';
